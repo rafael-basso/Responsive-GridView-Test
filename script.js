b05d6a48c41ee24a1b7da343d8f9bd172c9a1763
Rafael Basso <rafael_basso81@hotmail.com>
2021-02-06 20:04:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ function checkLogin(){
     var passwd01 = document.querySelector('.passwd01').value
     var passwd02 = document.querySelector('.passwd02').value
     var enterName = document.querySelector('.enterName').value
+    const msg = document.querySelector('.msg')
 
     if (enterName == "" || passwd01 =="" || passwd02 == "") {
         alert('Fill out all the fields')
-        return
     }
 
-    if (passwd01 === passwd02) {
-        window.open("./loginOk.html")
+    else if (passwd01 === passwd02) {
+        //window.open("./loginOk.html")
+        msg.innerHTML = `Name: ${enterName}` + '<br>' + `Password: ${passwd01}`
     }
     else {
         alert("Passwords don't match")
     }
 }
-
 
